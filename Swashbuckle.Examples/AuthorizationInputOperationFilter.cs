c1aff8eb7e344aec820a529bbd730561bda705ae
Matt Frear <matt.frear@gmail.com>
2017-09-02 14:42:44 +0100

    Updated readme, added Authorization input filter

---
+++ Swashbuckle.Examples/AuthorizationInputOperationFilter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Web.Http.Description;
+using Swashbuckle.Swagger;
+
+namespace Swashbuckle.Examples
+{
+    public class AuthorizationInputOperationFilter : IOperationFilter
+    {
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if (operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "access token",
+                required = false,
+                type = "string"
+            });
+        }
+    }
+}
